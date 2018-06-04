83f17decf1c312f6e085b14212e8bd19dcfb6f1e
santil01 <39658965+santil01@users.noreply.github.com>
2018-06-04 20:49:10 +0300

    Update telegrambot1.py

--- telegrambot1.py
+++ telegrambot1.py
@@ -1,79 +1,42 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
-import requests  
+import time
+import requests
 import datetime
+D = True 
+pull = {"Hello":"Hello Billionaire", "Hi":"hey","Time":time.strftime("%M")}
+datep = 0
+def getupdates():
+    s = requests.get("https://api.telegram.org/bot506337865:AAELIzsWuRF2Vw7HgcsHjATQd1SvH31Hr30/getupdates", proxies=dict(http='socks5://telegram:telegram@hvmas.tgproxy.me:1080', https='socks5://telegram:telegram@hvmas.tgproxy.me:1080'))
+    s = s.json()
+    date = s['result'][len(s['result'])-1]['message']['date']
+    chat_id = s['result'][len(s['result'])-1]['message']['chat']['id']
+    first_name = s['result'][len(s['result'])-1]['message']['from']['first_name']
+    s = s['result'][len(s['result'])-1]['message']['text']
+    return s, chat_id, date, first_name
+def sendm():
+    m = requests.get("https://api.telegram.org/bot506337865:AAELIzsWuRF2Vw7HgcsHjATQd1SvH31Hr30/sendMessage?chat_id=" + str(chat_id) + "&" + "text=" + str(textm), proxies=dict(http='socks5://telegram:telegram@hvmas.tgproxy.me:1080', https='socks5://telegram:telegram@hvmas.tgproxy.me:1080'))
+    
+while D:
+    
+    s, chat_id, date, first_name = getupdates()
+    min = time.strftime("%M")
+    if min=="00":
+        textm = "new hour!"
+        sendm()
+        time.sleep(60)
+    if date!=datep:     
 
-class BotHandler:
-
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-
-    def send_message(self, chat_id, text):
-        params = {'chat_id': chat_id, 'text': text}
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
-Последний штрих — объявим переменные и научим бота приличным манерам. Сделаем так, чтобы бот приветствовал вас раз в день, при этом фраза должна зависеть от времени суток. Для этого добавьте следующий код в ваш скрипт:
-
-greet_bot = BotHandler(token)  
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')  
-now = datetime.datetime.now()
-
-
-def main():  
-    new_offset = None
-    today = now.day
-    hour = now.hour
-
-    while True:
-        greet_bot.get_updates(new_offset)
-
-        last_update = greet_bot.get_last_update()
-
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
-
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
-
-        new_offset = last_update_id + 1
-
-if __name__ == '__main__':  
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
-
+        if s=="Btc":
+            b = requests.get("https://bittrex.com/api/v1.1/public/getmarketsummary?market=USD-BTC")
+            b = b.json()
+            b = b['result'][0]['Last']
+            textm = b
+            sendm()
+        for i in pull: 
+            if s==i:
+                textm = pull[i]
+                sendm()
+        datep = date
+        
+    
+    time.sleep(1)
+    
